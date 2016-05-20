df09f75a98cc1052133682c2aa1034a678e8d94c
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-05-20 23:02:58 +0530

    Update sublimetext_plugins.py

--- sublimetext_plugins.py
+++ sublimetext_plugins.py
@@ -1,1 +1,31 @@
+## Plugin to copy current file's path to clipboard
+import sublime
+import sublime_plugin
 
+
+class CopyPathToClipboard(sublime_plugin.TextCommand):
+    def run(self, edit):
+        line_number, column = self.view.rowcol(self.view.sel()[0].begin())
+        line_number += 1
+        #sublime.set_clipboard(self.view.file_name() + ':' + str(line_number))
+        sublime.set_clipboard(self.view.file_name())
+        
+# Past this code into file: copy_path_to_clipboard.py inside Sublime Text 3 Packages/User directory
+# Also get Main.sublime-menu file for running this plugin from Main menu "Vivek's Commands"
+
+## Plugin to copy current file's name to clipboard
+import sublime
+import sublime_plugin
+import ntpath
+
+
+class CopyFilenameToClipboard(sublime_plugin.TextCommand):
+    def run(self, edit):
+        line_number, column = self.view.rowcol(self.view.sel()[0].begin())
+        line_number += 1
+        currFileName = ntpath.basename(self.view.file_name())
+        #sublime.set_clipboard(self.view.file_name() + ':' + str(line_number))
+        sublime.set_clipboard(currFileName)
+        
+# Past this code into file: copy_filename_to_clipboard.py inside Sublime Text 3 Packages/User directory
+# Also get Main.sublime-menu file for running this plugin from Main menu "Vivek's Commands"
